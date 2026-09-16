28ace89407393cadd9e3b7e34f0a648ed36d3a39
Filipe Ribeiro <filribeiro@gmail.com>
2026-09-16 09:39:16 +0100

    Publica PPEPlan (ppeplan 14ac8b7)

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 
 // Substituído pelo Publicar-App.ps1 em cada publicação com alterações à app:
 // muda os bytes deste ficheiro e a app mostra "Há uma versão nova".
-const BUILD = "EE98AA264202";
+const BUILD = "673FD6372073";
 const CACHE_NAME = "ppeplan-shell";
 const APP_SHELL = [
   "./",
